e39c00cd693e96f670c95a60446d3abfbb946bc8
Dylan Pierce <dylanpierce914@gmail.com>
2019-03-09 10:46:23 -0800

    Smart mirror portfolio

---
+++ assets/js/smart_mirror_portfolio.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.slick').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    adaptiveHeight: true
+  });
+});
+
